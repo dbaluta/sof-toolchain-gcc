e4f1e1c3b18650d5fbe128ca471f65f1a8b02c41
Mihai Lindner <mihai.lindner@nxp.com>
2020-03-31 12:01:42 +0300

    YOCIMX-4113: Add gcc toolchain binaries and headers for xtensa
    Signed-off-by: Mihai Lindner <mihai.lindner@nxp.com>

---
+++ xtensa-imx-elf/lib/gcc/xtensa-imx-elf/8.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/sof/crosstool-ng/.build/xtensa-imx-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=xtensa-imx-elf --prefix=/home/sof/work/xtensa-imx-elf --with-local-prefix=/home/sof/work/xtensa-imx-elf/xtensa-imx-elf --with-headers=/home/sof/work/xtensa-imx-elf/xtensa-imx-elf/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.23.0.440-3b8c' --disable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/home/sof/crosstool-ng/.build/xtensa-imx-elf/buildtools --with-mpfr=/home/sof/crosstool-ng/.build/xtensa-imx-elf/buildtools --with-mpc=/home/sof/crosstool-ng/.build/xtensa-imx-elf/buildtools --with-isl=/home/sof/crosstool-ng/.build/xtensa-imx-elf/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
---
+++ xtensa-imx8m-elf/lib/gcc/xtensa-imx8m-elf/8.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/sof/crosstool-ng/.build/xtensa-imx8m-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=xtensa-imx8m-elf --prefix=/home/sof/work/xtensa-imx8m-elf --with-local-prefix=/home/sof/work/xtensa-imx8m-elf/xtensa-imx8m-elf --with-headers=/home/sof/work/xtensa-imx8m-elf/xtensa-imx8m-elf/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.23.0.440-3b8c' --disable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/home/sof/crosstool-ng/.build/xtensa-imx8m-elf/buildtools --with-mpfr=/home/sof/crosstool-ng/.build/xtensa-imx8m-elf/buildtools --with-mpc=/home/sof/crosstool-ng/.build/xtensa-imx8m-elf/buildtools --with-isl=/home/sof/crosstool-ng/.build/xtensa-imx8m-elf/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
